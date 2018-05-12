734e33ed5f92bec94b8745792039cc10bfb25978
Miguel Fletes <mfletes@csumb.edu>
2018-05-12 23:59:50 +0000

    connected lab9 to database

---
+++ connect.php
@@ -0,0 +1,38 @@
+<?php
+
+function getDBConnection() {
+    
+    //C9 db info
+    $host = "localhost";
+    $dbName = "csumb_quiz";
+    //$username =  "root";
+    //$password = "cst336";
+     $username = "test";
+     $password = "ljihu38HkU3mMbjy";
+    
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+    
+    try {
+        //Creates a database connection
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    
+        // Setting Errorhandling to Exception
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $e) {
+       echo "Problems connecting to database!";
+       exit();
+    }
+    
+    
+    return $dbConn;
+}
+
+?>
